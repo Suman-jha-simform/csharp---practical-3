b7d82c254468c679a8d2ff2f6e39b79f1aea2684
suman jha <suman.j@simformsolutions.com>
2023-03-15 15:18:33 +0530

    add practical 3 polymorphism

---
+++ practical 3 - polymorphism/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Bird
+    {
+        /// <summary>
+        /// This method prints the voice of a bird.
+        /// </summary>
+        /// <returns>void</returns>
+        public void Voice()
+        {
+            Console.WriteLine("Turr Turr");
+        }
+    }
+
+    class Duck : Bird
+    {
+        /// <summary>
+        /// This method prints the voice of a Duck.
+        /// </summary>
+        /// <returns>void</returns>
+        public void Voice()
+        {
+            Console.WriteLine("Quack Quack");
+        }
+
+        public static void Main()
+        {
+            Bird myBird = new Bird();
+            //Duck myDuck = new Bird();
+
+            Bird myBird2 = new Duck();
+
+            myBird.Voice();
+            myBird2.Voice();
+        }
+    }
+}
