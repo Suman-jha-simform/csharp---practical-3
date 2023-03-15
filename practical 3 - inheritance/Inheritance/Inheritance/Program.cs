13fa4b90ce1c12bc986ec475100fb95159bb6cce
suman jha <suman.j@simformsolutions.com>
2023-03-15 15:18:11 +0530

    add practical 3 inheritance

---
+++ practical 3 - inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Sponsor
+    {
+        protected string owner = "Rakuteen";
+    }
+    class Team : Sponsor
+    {
+        private string _teamName;
+
+        public Team(string teamname)
+        {
+            this._teamName= teamname;
+        }
+        /// <summary>
+        /// This method returns the concated value of owner and teamname varibales.
+        /// </summary>
+        /// <returns>string</returns>
+        public string PrintInfo()
+        {
+            return $"{owner} {_teamName}";
+        }
+
+        public static void Main()
+        {
+            Team team = new Team("Lions");
+            string ownerandteam = team.PrintInfo();
+            Console.WriteLine(ownerandteam);
+        }
+    }
+}
